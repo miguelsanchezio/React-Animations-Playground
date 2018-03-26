4c4acf6925b93a4c19ed3d09fe1a121c733a9507
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-26 10:55:18 -0700

    The modal now animates using Transition.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
-const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
-    
-    return (
-        <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
-    )
+const modal = props => {
+	
+	return (
+		<Transition
+			mountOnEnter
+			unmountOnExit
+			in={props.show}
+			timeout={300}>
+			{state => {
+				const cssClasses = [
+					'Modal',
+					state === 'entering'
+						? 'ModalOpen'
+						: state === 'exiting' ? 'ModalClosed' : null
+				]
+
+				return (
+					<div className={cssClasses.join(' ')}>
+						<h1>A Modal</h1>
+						<button className="Button" onClick={props.closed}>Dismiss</button>
+					</div>
+				)
+			}}
+		</Transition>
+	)
 }
 
 export default modal;
